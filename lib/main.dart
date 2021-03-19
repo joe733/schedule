c6562ca5d7b29333e154c1720ef6eed94908ff63
joe733 <jovial7joe@hotmail.com>
2021-03-19 10:46:20 +0530

    feat: completes basic layout  🥳

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Schedule',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Schedule'),
@@ -30,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,57 +27,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Text(
+                'S8 CSE',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 50),
               Row(
+                  //General Row
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    //General Row
                     myButtons(Icons.add_to_drive, 'Drive',
                         'https://drive.google.com/drive/folders/1oOzkR6gugx8ZtgM6SSqTDz8xpMav_v8U'),
                     SizedBox(width: 10),
@@ -105,6 +53,36 @@ class _MyHomePageState extends State<MyHomePage> {
                         'mailto:iesbtechcs2k17to21@googlegroups.com'),
                     SizedBox(width: 10),
                   ]),
+              SizedBox(height: 20),
+              Row(
+                  //Row 1
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    myButtons(Icons.meeting_room_rounded, 'EIA',
+                        'https://meet.google.com/ecd-obdy-zgd'),
+                    SizedBox(width: 10),
+                    myButtons(Icons.meeting_room_rounded, 'DMW',
+                        'https://meet.google.com/jce-fwot-zqb'),
+                    SizedBox(width: 10),
+                    myButtons(Icons.meeting_room_rounded, 'ES',
+                        'https://meet.google.com/yja-kdxa-ixc'),
+                    SizedBox(width: 10),
+                  ]),
+              SizedBox(height: 20),
+              Row(
+                  //Row 2
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    myButtons(Icons.meeting_room_rounded, 'AI',
+                        'https://meet.google.com/erc-dsjk-iez'),
+                    SizedBox(width: 10),
+                    myButtons(Icons.meeting_room_rounded, 'PIS',
+                        'https://meet.google.com/cao-gkxq-mox'),
+                    SizedBox(width: 10),
+                    myButtons(Icons.meeting_room_rounded, 'Project',
+                        'https://meet.google.com/yzg-ccdf-qdm'),
+                    SizedBox(width: 10),
+                  ])
             ]),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
